fbd3e97d73d6dabc24872ba586ca66c317a0a1d1
Nivando Soares <sidvandoni@gmail.com>
2026-02-21 09:50:01 -0300

    Add entrypoint reference map for guided SNES disassembly

---
+++ reconstructed/code/rom_layout.asm
@@ -0,0 +1,134 @@
+; Auto-gerado para facilitar engenharia reversa.
+; Os bancos foram extraídos para análise em ferramentas como bsnes-plus, Mesen-S, Ghidra e IDA.
+.cpu 65816
+
+; Mapeamento detectado: LoROM
+; Título: THE DUEL: TEST DRIVE
+
+.segment "BANK00"
+.org $8000
+.incbin "banks/bank_00.bin"
+
+.segment "BANK01"
+.org $8000
+.incbin "banks/bank_01.bin"
+
+.segment "BANK02"
+.org $8000
+.incbin "banks/bank_02.bin"
+
+.segment "BANK03"
+.org $8000
+.incbin "banks/bank_03.bin"
+
+.segment "BANK04"
+.org $8000
+.incbin "banks/bank_04.bin"
+
+.segment "BANK05"
+.org $8000
+.incbin "banks/bank_05.bin"
+
+.segment "BANK06"
+.org $8000
+.incbin "banks/bank_06.bin"
+
+.segment "BANK07"
+.org $8000
+.incbin "banks/bank_07.bin"
+
+.segment "BANK08"
+.org $8000
+.incbin "banks/bank_08.bin"
+
+.segment "BANK09"
+.org $8000
+.incbin "banks/bank_09.bin"
+
+.segment "BANK0A"
+.org $8000
+.incbin "banks/bank_0A.bin"
+
+.segment "BANK0B"
+.org $8000
+.incbin "banks/bank_0B.bin"
+
+.segment "BANK0C"
+.org $8000
+.incbin "banks/bank_0C.bin"
+
+.segment "BANK0D"
+.org $8000
+.incbin "banks/bank_0D.bin"
+
+.segment "BANK0E"
+.org $8000
+.incbin "banks/bank_0E.bin"
+
+.segment "BANK0F"
+.org $8000
+.incbin "banks/bank_0F.bin"
+
+.segment "BANK10"
+.org $8000
+.incbin "banks/bank_10.bin"
+
+.segment "BANK11"
+.org $8000
+.incbin "banks/bank_11.bin"
+
+.segment "BANK12"
+.org $8000
+.incbin "banks/bank_12.bin"
+
+.segment "BANK13"
+.org $8000
+.incbin "banks/bank_13.bin"
+
+.segment "BANK14"
+.org $8000
+.incbin "banks/bank_14.bin"
+
+.segment "BANK15"
+.org $8000
+.incbin "banks/bank_15.bin"
+
+.segment "BANK16"
+.org $8000
+.incbin "banks/bank_16.bin"
+
+.segment "BANK17"
+.org $8000
+.incbin "banks/bank_17.bin"
+
+.segment "BANK18"
+.org $8000
+.incbin "banks/bank_18.bin"
+
+.segment "BANK19"
+.org $8000
+.incbin "banks/bank_19.bin"
+
+.segment "BANK1A"
+.org $8000
+.incbin "banks/bank_1A.bin"
+
+.segment "BANK1B"
+.org $8000
+.incbin "banks/bank_1B.bin"
+
+.segment "BANK1C"
+.org $8000
+.incbin "banks/bank_1C.bin"
+
+.segment "BANK1D"
+.org $8000
+.incbin "banks/bank_1D.bin"
+
+.segment "BANK1E"
+.org $8000
+.incbin "banks/bank_1E.bin"
+
+.segment "BANK1F"
+.org $8000
+.incbin "banks/bank_1F.bin"
